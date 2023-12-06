3b876a1da16ee2fb7f1719f68a2ac3225ad0fd07
omerh <omerh@DESKTOP-U09I3KP>
2023-12-06 22:12:51 +0200

    change ip and port to Render

--- Backend/main.py
+++ Backend/main.py
@@ -54,4 +54,5 @@ async def predict(file: UploadFile = File(...)):
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=80)
+
