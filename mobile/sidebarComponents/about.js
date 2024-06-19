011887126ce203dcdf137fa3fd1f52893dec914e
omerh <omerh@DESKTOP-U09I3KP>
2024-06-19 22:35:43 +0300

    Add link for LinkedIn from the images

--- mobile/sidebarComponents/about.js
+++ mobile/sidebarComponents/about.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import {Text, View, StyleSheet,Image} from "react-native";
+import React, {useState} from 'react';
+import {Text, View, StyleSheet, Image, TouchableOpacity, Linking, Alert} from "react-native";
 import Logo from "../Home/logo";
 import Sidebar from "../Sidebar";
+import { WebView } from 'react-native-webview';
+import Modal from "react-native-modal";
+import EntypoIcon from "react-native-vector-icons/Entypo";
+
 
 const About = () =>{
+    const [modalVisible, setModalVisible] = React.useState(false);
+    const [uri, setUri] = useState('');
+     function handleGuy() {
+        setModalVisible(true);
+        setUri('https://www.linkedin.com/in/guy-shabtay/');
+    }
+
+    function handleOmer() {
+        setModalVisible(true);
+        setUri('https://www.linkedin.com/in/omer-halfon-ab0b51235/');
+    }
+
+
 
     return(
         <View style={styles.container}>
@@ -16,11 +33,23 @@ const About = () =>{
                     {'\n'}
                     We believe in the power of collaboration, continuous learning, and staying at the forefront of industry trends. Our values include integrity, excellence, and a commitment to customer satisfaction.{'\n'}
                 </Text>
-                <Image source={require('../images/omer.jpg')} style={styles.image}/>
+                <TouchableOpacity onPress={handleOmer}>
+                    <Image source={require('../images/omer.jpg')} style={styles.image} />
+                </TouchableOpacity>
                 <Text>Omer Halfon</Text>
-                <Image source={require('../images/guy.jpg')} style={styles.image}/>
+                <TouchableOpacity onPress={handleGuy}>
+                    <Image source={require('../images/guy.jpg')} style={styles.image} />
+                </TouchableOpacity>
                 <Text>Guy Shabtay</Text>
             </View>
+
+            <Modal
+                visible={modalVisible}
+                onRequestClose={() => setModalVisible(false)}
+                animationType="slide"
+            >
+                <WebView source={{ uri: uri }} />
+            </Modal>
         </View>
     );
 };
